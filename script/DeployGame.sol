dbace6793cfb4078a22d9358a3de0e5b1e5e2a2a
MBrock_29 <mbrock19@gmail.com>
2024-09-13 15:11:01 +0100

    Installing FE and alchemy setup

---
+++ script/DeployGame.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+import "forge-std/Script.sol";
+import "../src/Game.sol"; 
+
+contract DeployGame is Script {
+    function run() external {
+        vm.startBroadcast();
+
+        Game game = new Game();
+        vm.stopBroadcast();
+    }
+}
